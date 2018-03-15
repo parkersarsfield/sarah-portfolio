991cd6d268b29a108df3b966c5c663b97eee221e
Parker Sarsfield <parker@Parkers-MacBook-Pro.local>
2018-03-15 17:28:35 -0500

    Fix contact page.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,22 +1,75 @@
-import React from 'react'
+import React from "react";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
 
-const ContactPage = () => (
-    <div>
-        <form name="contact" method="POST" netlify>
-            <p>
-                <label>Your Name: <input type="text" name="name" /></label>
-            </p>
-            <p>
-                <label>Your Email: <input type="email" name="email" /></label>
-            </p>
-            <p>
-                <label>Message: <textarea name="message"></textarea></label>
-            </p>
-            <p>
-                <button type="submit">Send</button>
-            </p>
-        </form>
-    </div>
-)
+function encode(data) {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+}
 
-export default ContactPage
+export default class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    handleSubmit = e => {
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", ...this.state })
+        })
+            .then(() => alert("Success!"))
+            .catch(error => alert(error));
+
+        e.preventDefault();
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Contact</h1>
+                <form
+                    name="contact"
+                    method="post"
+                    action="/thanks/"
+                    data-netlify="true"
+                    data-netlify-honeypot="bot-field"
+                    onSubmit={this.handleSubmit}
+                >
+                    <p hidden>
+                        <label>
+                            Don’t fill this out: <input name="bot-field" />
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            Your name:<br />
+                            <input type="text" name="name" onChange={this.handleChange} />
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            Your email:<br />
+                            <input type="email" name="email" onChange={this.handleChange} />
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            Message:<br />
+                            <textarea name="message" onChange={this.handleChange} />
+                        </label>
+                    </p>
+                    <p>
+                        <button type="submit">Send</button>
+                    </p>
+                </form>
+            </div>
+        );
+    }
+}
