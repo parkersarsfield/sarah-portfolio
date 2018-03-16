2f712bac9f4fb816cf4f8d19f280bf7829ab6cc2
Parker Sarsfield <parker@Parkers-MacBook-Pro.local>
2018-03-16 01:10:25 -0500

    Added more fluff.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -13,16 +13,15 @@ const instagramUrl = 'https://www.instagram.com/saraheborton/'
 const Footer = () => {
     return (
         <div className="footer-container">
-        <div className="footer">
-            <Link className="footer_link" to='/contact'>contact</Link>
-            <a className="footer_link" href={instagramUrl}><FontAwesomeIcon icon={["fab", "instagram"]} /></a>
-            <Link className="footer_link" to='/about'>about</Link>
+            <div className="footer">
+                <Link className="footer_link" to='/contact'>contact</Link>
+                <a className="footer_link" href={instagramUrl}><FontAwesomeIcon icon={["fab", "instagram"]} /></a>
+                <Link className="footer_link" to='/about'>about</Link>
+            </div>
+            <div className="legal">
+                All images and content property of Sarah Borton.
+            </div>
         </div>
-        <div className="legal">
-            All images and content property of Sarah Borton.
-        </div>
-        </div>
-
     )
 }
 
