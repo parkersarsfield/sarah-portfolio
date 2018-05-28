6e4ee2c680af41ba638ddec43762d95849010dde
parkersarsfield <phsarsfield@gmail.com>
2018-05-28 18:20:07 -0500

    Add social media and add support for png and jpg on social page.

--- src/pages/socialmedia/index.js
+++ src/pages/socialmedia/index.js
@@ -1,37 +1,63 @@
 import React from 'react'
 import PortfolioList from '../../components/PortfolioList'
 
+const formatImageObjects = data => {
+  const items = data.edges.map(item => {
+    return {
+      sizes: item.node.childImageSharp.sizes,
+      name: item.node.name
+    }
+  })
+
+  return items
+}
 
 const SocialPage = ({ data }) => {
-    const items = data.allFile.edges.map(item => {
-        return {
-            sizes: item.node.childImageSharp.sizes,
-            name: item.node.name
-        }
-    })
-    
-    return (
-        <PortfolioList items={items} title="social media" />
-    )
+  console.log(data)
+  const items = formatImageObjects(data.allPng).concat(
+    formatImageObjects(data.allJpg)
+  )
+
+  return <PortfolioList items={items} title="social media" />
 }
 
 export default SocialPage
 
 export const query = graphql`
-query SocialQuery {
-    allFile(filter: {
-        extension: {eq: "jpg"},
-        absolutePath: {regex: "/(social)/"}}) {
+  query SocialQuery {
+    allJpg: allFile(
+      filter: {
+        extension: { eq: "jpg" }
+        absolutePath: { regex: "/(social)/" }
+      }
+    ) {
       edges {
         node {
           id
           childImageSharp {
-            sizes (maxWidth: 640) {
-                ...GatsbyImageSharpSizes
+            sizes(maxWidth: 640) {
+              ...GatsbyImageSharpSizes
+            }
+          }
+        }
+      }
+    }
+    allPng: allFile(
+      filter: {
+        extension: { eq: "png" }
+        absolutePath: { regex: "/(social)/" }
+      }
+    ) {
+      edges {
+        node {
+          id
+          childImageSharp {
+            sizes(maxWidth: 640) {
+              ...GatsbyImageSharpSizes
             }
           }
         }
       }
     }
   }
-  `
+`
